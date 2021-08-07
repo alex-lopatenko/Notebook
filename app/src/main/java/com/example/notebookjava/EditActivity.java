f30549a17b6ac087d4445ada4a859d7161ec7a21
Alexey <alexey-lopatenko@yandex.ru>
2021-08-07 19:46:16 +0300

    Deploy 5

--- app/src/main/java/com/example/notebookjava/EditActivity.java
+++ app/src/main/java/com/example/notebookjava/EditActivity.java
@@ -1,8 +1,10 @@
 package com.example.notebookjava;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -14,12 +16,14 @@ import com.example.notebookjava.db.MyDbManager;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EditActivity extends AppCompatActivity {
+    private final int PICK_IMAGE_CODE = 123;
     private ImageView imNewImage;
     private ConstraintLayout imageContainer;
     private FloatingActionButton fbAddImage;
     //private ImageButton imEditImage, imDeleteImage;
     private EditActivity edTitle, edDesc;
     private myDbManager myDbManager;
+    private String tempUri = "empty";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +36,20 @@ public class EditActivity extends AppCompatActivity {
         super.onResume();
         myDbManager.openDb();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_OK && requestCode == PICK_IMAGE_CODE && data != null) {
+            tempUri = data.getData().toString();
+            imNewImage.setImageURI(data.getData());
+        }
+    }
+
     private void init(){
         edTitle = findViewById(R.id.edTitle);
         edDesc = findViewById(R.id.edDesc);
+        imNewImage = findViewById(R.id.imNewImage);
         fbAddImage = findViewById(R.id.fbAddImage);
         imageContainer = findViewById(R.id.imageContainer);
         myDbManager = new MyDbManager(this);
@@ -47,13 +62,15 @@ public class EditActivity extends AppCompatActivity {
         if (title.equals("") || desc.equals("")) {
             Toast.makeText(this, R.string.text_empty, Toast.LENGTH_SHORT).show();
         } else {
-            myDbManager.insertToDb(title, desc);
+            myDbManager.insertToDb(title, desc, tempUri);
             Toast.makeText(this, R.string.saved, Toast.LENGTH_SHORT).show();
             finish();
             myDbManager.closeDb();
         }
     }
     public void onClickDeleteImage(View view) {
+        imNewImage.setImageResource(R.drawable.ic_image_def);
+        tempUri = "empty";
         imageContainer.setVisibility(View.GONE);
         view.setVisibility(View.VISIBLE);
     }
@@ -61,5 +78,11 @@ public class EditActivity extends AppCompatActivity {
     public void onClickAddImage(View view) {
         imageContainer.setVisibility(View.VISIBLE);
         view.setVisibility(View.GONE);
+    }
+
+    public void onClickChooseImage(View view) {
+        Intent chooser = new Intent(Intent.ACTION_PICK);
+        chooser.setType("image/*");
+        startActivityForResult(chooser, PICK_IMAGE_CODE);
     }
 }
